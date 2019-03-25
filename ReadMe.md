ebb27f530bf86f63ed784e9f0291dafd6e4a7ad6
SablinIgor <sablin.igor@gmail.com>
2019-03-25 20:24:20 +0300

    VPN

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,22 @@
 One line: ssh -At -i ~/.ssh/ubuntu -A ubuntu@35.233.72.169 ssh 10.132.0.3
 
+Убрать проверку на хост
+
+~/.ssh/config
+Host bastion
+    HostName 35.233.72.169
+    User ubuntu
+
+Host someinternalhost 10.132.0.28
+    User ubuntu
+    ProxyCommand ssh -i ~/.ssh/ubuntu -A bastion nc -w 180 %h %p% 
+
+
+    
+
+Посмотреть что получилось
+    terraform show
+
+Скачать скрипт
+    curl https://gist.githubusercontent.com/Nklya/df07e99e63e4043e6a699060a7e30b66/raw/d5d572755ea811ac7d4e1b7b47d5f0c9527693b3/setupvpn.sh | bash
+
--- bastion.tf
+++ bastion.tf
@@ -9,6 +9,7 @@ resource "google_compute_address" "static" {
 resource "google_compute_instance" "vm_instance" {
   name         = "bastion"
   machine_type = "f1-micro"
+  tags = ["http-server","https-server"]
 
   boot_disk {
     initialize_params {
@@ -22,10 +23,23 @@ resource "google_compute_instance" "vm_instance" {
             nat_ip = "${google_compute_address.static.address}"
         }
     }
+  provisioner "remote-exec" {
+    inline = [
+      "curl https://gist.githubusercontent.com/Nklya/df07e99e63e4043e6a699060a7e30b66/raw/d5d572755ea811ac7d4e1b7b47d5f0c9527693b3/setupvpn.sh | bash",
+      "sudo bash setupvpn.sh"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      private_key = "${file("/Users/admin/.ssh/ubuntu")}"
+    }
+
+  }
 }
 
 resource "google_compute_firewall" "allow_http" {  
-    name = "allow-http"
+    name = "default-allow-http"
     network = "default"
 
     allow {
@@ -34,11 +48,11 @@ resource "google_compute_firewall" "allow_http" {
     }
 
     source_ranges = ["0.0.0.0/0"]
-    target_tags = ["http"]
+    target_tags = ["http-server"]
 }
 
 resource "google_compute_firewall" "allow_https" {  
-    name = "allow-https"
+    name = "default-allow-https"
     network = "default"
 
     allow {
@@ -47,5 +61,5 @@ resource "google_compute_firewall" "allow_https" {
     }
 
     source_ranges = ["0.0.0.0/0"]
-    target_tags = ["https"]
+    target_tags = ["https-server"]
 }
